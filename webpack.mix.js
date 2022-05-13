ba2f57401729c845ae3a14028a0323f7798d4c69
salehhashemi1992 <salpars2004@gmail.com>
2022-05-13 20:04:44 +0430

    Jquery added to the project and boostrap.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-mix.js('resources/js/admin.js', 'public/theme/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .sass('resources/sass/admin.scss', 'public/theme/css')
     .sourceMaps();
